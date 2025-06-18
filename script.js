0d25715d643273682b24e1b271a9fe38a8cf3f38
georgevfc13 <georgeyeste@gmail.com>
2025-06-18 14:36:05 -0500

    mejora del boton del video

---
+++ script.js
@@ -0,0 +1,13 @@
+ document.getElementById('btn-play').addEventListener('click', function() {
+    // Oculta la miniatura y el botón
+    document.getElementById('miniatura-video').style.opacity = 0;
+    this.style.opacity = 0;
+    setTimeout(() => {
+      document.getElementById('miniatura-video').style.display = 'none';
+      document.getElementById('btn-play').style.display = 'none';
+      // Muestra el video
+      const video = document.getElementById('video-container');
+      video.style.pointerEvents = 'auto';
+      video.style.opacity = 1;
+    }, 400);
+  });
